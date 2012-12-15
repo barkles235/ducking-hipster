3778d06103a2be3e2beac4cc16a0937575fd42da
brian <brian@zesty.org>
2012-12-15 14:06:13 -0500

    Posts belong_to Users

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,3 +1,4 @@
 class User < ActiveRecord::Base
   attr_accessible :bio, :name, :password, :password_digest
+  has_many :posts
 end
--- db/schema.rb
+++ db/schema.rb
@@ -11,7 +11,14 @@
 #
 # It's strongly recommended to check this file into your version control system.
 
-ActiveRecord::Schema.define(:version => 20121215184314) do
+ActiveRecord::Schema.define(:version => 20121215190159) do
+
+  create_table "posts", :force => true do |t|
+    t.text     "body"
+    t.datetime "created_at", :null => false
+    t.datetime "updated_at", :null => false
+    t.integer  "user_id"
+  end
 
   create_table "users", :force => true do |t|
     t.string   "name"
